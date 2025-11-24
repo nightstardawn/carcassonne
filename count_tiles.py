ab14560e3896567904298c074712940ae34bb8e5
Zac Garby <me@zacgarby.co.uk>
2025-11-24 14:06:24 +0000

    counttiles

---
+++ count_tiles.py
@@ -0,0 +1,52 @@
+from itertools import product
+from typing import get_args
+from geom import *
+from tileset import TileKind, Tileset
+
+
+# returns (actual, max possible)
+def count(tiles: Tileset) -> tuple[int, int]:
+    possibilities = [
+        {U: u, D: d, L: l, R: r}
+        for (u, d, l, r) in product(["city", "road", None], repeat=4)
+    ]
+
+    n = 0
+    for poss in possibilities:
+        try:
+            match = next(kind for kind in tiles.kinds if matches(kind, poss))
+            n += 1
+        except StopIteration:
+            print(f"nothing matches {poss}")
+
+    return (n, len(possibilities))
+
+def matches(kind: TileKind, sides: dict[Direction, str | None]) -> bool:
+    for angle in get_args(Angle):
+        this_matches = True
+
+        for dir, side in sides.items():
+            r_dir = dir.rotate(angle)
+
+            if side == "road" and r_dir not in kind.roads:
+                this_matches = False
+                break
+
+            if side == "city" and all(r_dir not in city for city in kind.cities):
+                this_matches = False
+                break
+
+            if side == None and (any(r_dir in city for city in kind.cities) or r_dir in kind.roads):
+                this_matches = False
+                break
+
+        if this_matches:
+            return True
+
+    return False
+
+tileset = Tileset(Tileset.BaseTiles)
+
+actual, possible = count(tileset)
+print(f"in the base tileset, there are {actual} distinct tile layouts, out of a possible {possible}")
+print("(modulo shields, monasteries, and rivers; we only care about roads and cities here. we also don't consider the connectness of cities and roads)")
